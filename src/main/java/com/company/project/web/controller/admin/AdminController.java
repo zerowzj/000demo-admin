39598b30fe1b1d312de3a7cd763e36f88a398dcb
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2017-03-01 16:05:00 +0800

    更新代码

--- src/main/java/com/company/project/web/controller/admin/AdminController.java
+++ src/main/java/com/company/project/web/controller/admin/AdminController.java
@@ -1,16 +1,13 @@
 package com.company.project.web.controller.admin;
 
 import com.company.project.service.admin.AdminService;
-import com.company.project.web.pattern.action.ActionExecutor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Map;
 
 /**
  * 管理员控制器
@@ -37,11 +34,5 @@ public class AdminController {
     @RequestMapping("/toList")
     public ModelAndView toList(HttpServletRequest request, HttpServletResponse response) {
         return new ModelAndView("/admin/adminList");
-    }
-
-    @RequestMapping("/list")
-    @ResponseBody
-    public Map<String, Object> list(HttpServletRequest request, HttpServletResponse response) {
-        return ActionExecutor.execute(request, response, Action_admin_list.class);
     }
 }
